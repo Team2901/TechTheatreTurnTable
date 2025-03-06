caf914d59962bb035b90a46ec7771f6032036aab
John Nicholson <jnichol@lenovo.com>
2025-03-06 16:39:20 -0500

    Fixes / tested and working
    The adjustableValues was getting a copy of the Double instead of the reference. Maybe this could be made to work, but instead I just switched to using a HashMap. Originally I didn't want to use the HashMap because of maybe not being able to control the order of cycling through values, but we can come back to that later if needed.
    
    For gamepad, was copying in the wrong direction. It is a copyFrom but I assumed it was a copyTo.
    
    Added ability to force the motors on or off, irrespective of the analog signal. This logic isn't probably exactly what we want. If the analog signal switches it will take back control.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechTheatreTurnTable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechTheatreTurnTable.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @TeleOp(name="TurnTable", group="TechTheatre")
@@ -18,27 +21,31 @@ public class TechTheatreTurnTable extends OpMode
     DcMotorEx motor2;
 
     static double TICKS_PER_REV = 1425.1;
-    Double setSpeed = 1.0;
-    Double signalThreshold = 2.5; // Volts
 
     boolean previousSignal = false;
-    Gamepad previousGamepad;
+    Gamepad previousGamepad = new Gamepad();
 
     Double delta = 1.0;
 
-    private static class AdjustableValue
+    public enum AdjustableValue
     {
-        String label;
-        Double value;
-        public AdjustableValue(String label, Double value) {
-            this.label = label;
-            this.value = value;
-        }
+        SetSpeed,
+        SignalThreshold,
     }
-    private final AdjustableValue[] adjustableValues = new AdjustableValue[]{
-        new AdjustableValue("Set Speed", setSpeed),
-        new AdjustableValue("Signal Threshold", signalThreshold)};
-    int adjustableValueIndex = 0;
+    public static AdjustableValue getNextAdjustableValue(AdjustableValue current) {
+        AdjustableValue[] values = AdjustableValue.values();
+        int nextOrdinal = (current.ordinal() + 1) % values.length;
+        return values[nextOrdinal];
+    }
+    public static AdjustableValue getPrevAdjustableValue(AdjustableValue current) {
+        AdjustableValue[] values = AdjustableValue.values();
+        int nextOrdinal = (current.ordinal() - 1 + values.length) % values.length;
+        return values[nextOrdinal];
+    }
+    private final Map<AdjustableValue,Double> adjustableValues = new HashMap<>(Map.ofEntries(
+        Map.entry(AdjustableValue.SetSpeed, 1.0),
+        Map.entry(AdjustableValue.SignalThreshold, 2.5)));
+    AdjustableValue currentlyAdjustingValue = AdjustableValue.SetSpeed;
 
     @Override
     public void init() {
@@ -47,8 +54,11 @@ public class TechTheatreTurnTable extends OpMode
         motor0 = hardwareMap.tryGet(DcMotorEx.class, "motor0");
         motor1 = hardwareMap.tryGet(DcMotorEx.class, "motor1");
         motor2 = hardwareMap.tryGet(DcMotorEx.class, "motor2");
+        setMotorMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        setMotorZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        setMotorSpeed(0.0);
 
-        gamepad1.copy(previousGamepad);
+        previousGamepad.copy(gamepad1);
 
         if (controlSignal == null) {
             telemetry.addLine("Error: control signal is missing from hardwareMap!");
@@ -67,25 +77,15 @@ public class TechTheatreTurnTable extends OpMode
             return;
         }
 
-        // The signal is active high -- when the voltage is high, the motor should be on
-        double currentSignalVoltage = controlSignal.getVoltage();
-        boolean currentSignal = currentSignalVoltage > signalThreshold;
-
-        // If there is a change in the signal, change the behavior
-        if (currentSignal != previousSignal) {
-            if (currentSignal) {
-                setMotorSpeed(setSpeed);
-            } else {
-                setMotorSpeed(0);
-            }
-        }
-
         // User controls
+        boolean valueChanged = false;
         if (gamepad1.dpad_up && !previousGamepad.dpad_up) {
-            adjustableValues[adjustableValueIndex].value += delta;
+            adjustableValues.replace(currentlyAdjustingValue, adjustableValues.get(currentlyAdjustingValue) + delta);
+            valueChanged = true;
         }
         if (gamepad1.dpad_down && !previousGamepad.dpad_down) {
-            adjustableValues[adjustableValueIndex].value -= delta;
+            adjustableValues.replace(currentlyAdjustingValue, adjustableValues.get(currentlyAdjustingValue) - delta);
+            valueChanged = true;
         }
         if (gamepad1.dpad_left && !previousGamepad.dpad_left) {
             delta /= 10;
@@ -94,13 +94,37 @@ public class TechTheatreTurnTable extends OpMode
             delta *= 10;
         }
         if (gamepad1.left_bumper && !previousGamepad.left_bumper) {
-            adjustableValueIndex = (adjustableValueIndex - 1 + adjustableValues.length) % adjustableValues.length;
+            //adjustableValueIndex = (adjustableValueIndex - 1 + adjustableValues.length) % adjustableValues.length;
+            currentlyAdjustingValue = getNextAdjustableValue(currentlyAdjustingValue);
         }
         if (gamepad1.right_bumper && !previousGamepad.right_bumper) {
-            adjustableValueIndex = (adjustableValueIndex + 1 ) % adjustableValues.length;
+            //adjustableValueIndex = (adjustableValueIndex + 1 ) % adjustableValues.length;
+            currentlyAdjustingValue = getPrevAdjustableValue(currentlyAdjustingValue);
+        }
+        boolean forceOn = false;
+        if (gamepad1.a && !previousGamepad.a) {
+            forceOn = true;
+        }
+        boolean forceStop = false;
+        if (gamepad1.b && !previousGamepad.b) {
+            forceStop = true;
+        }
+
+
+        // The signal is active high -- when the voltage is high, the motor should be on
+        double currentSignalVoltage = controlSignal.getVoltage();
+        boolean currentSignal = currentSignalVoltage > adjustableValues.get(AdjustableValue.SignalThreshold);
+
+        // If there is a change in the signal, or the user forces the motor change, change the behavior
+        if (((currentSignal != previousSignal) && currentSignal) || forceOn || (currentSignal && valueChanged)) {
+            setMotorSpeed(adjustableValues.get(AdjustableValue.SetSpeed));
+        } else if (((currentSignal != previousSignal) && !currentSignal) || forceStop) {
+            setMotorSpeed(0);
         }
 
         // Print help for controls
+        telemetry.addData("a", "force on");
+        telemetry.addData("b", "force stop");
         telemetry.addData("dpad up", "increase speed by delta");
         telemetry.addData("dpad down", "decrease speed by delta");
         telemetry.addData("dpad left", "decrease delta by factor of 10");
@@ -108,22 +132,23 @@ public class TechTheatreTurnTable extends OpMode
         telemetry.addData("left bumper", "cycle left through adjustable values");
         telemetry.addData("right bumper", "cycle right through adjustable values");
         telemetry.addData("adjustable values",
-            Arrays.stream(adjustableValues).map(v -> v.label).collect(Collectors.joining(", ")));
-        telemetry.addData("adjusting value", adjustableValues[adjustableValueIndex].label);
+            //Arrays.stream(adjustableValues).map(v -> v.label).collect(Collectors.joining(", ")));
+            Arrays.stream(AdjustableValue.values()).map(Enum::name).collect(Collectors.joining(", ")));
+        telemetry.addData("adjusting value", currentlyAdjustingValue);
         telemetry.addData("delta", delta);
         telemetry.addLine("----------");
         // Print telemetry information
         telemetry.addData("signal", currentSignal);
         telemetry.addData("signal voltage", currentSignalVoltage);
-        telemetry.addData("signal threshold", signalThreshold);
-        telemetry.addData("set speed (rps)", setSpeed);
-        telemetry.addData("set speed (tps)", TICKS_PER_REV * setSpeed);
+        telemetry.addData("signal threshold", adjustableValues.get(AdjustableValue.SignalThreshold));
+        telemetry.addData("set speed (rps)", adjustableValues.get(AdjustableValue.SetSpeed));
+        telemetry.addData("set speed (tps)", TICKS_PER_REV * adjustableValues.get(AdjustableValue.SetSpeed));
         if (motor0 != null) telemetry.addData("current speed (0)", motor0.getVelocity());
         if (motor1 != null) telemetry.addData("current speed (1)", motor1.getVelocity());
         if (motor2 != null) telemetry.addData("current speed (2)", motor2.getVelocity());
 
         previousSignal = currentSignal;
-        gamepad1.copy(previousGamepad);
+        previousGamepad.copy(gamepad1);
     }
 
     @Override
@@ -135,5 +160,17 @@ public class TechTheatreTurnTable extends OpMode
         if (motor0 != null) motor0.setVelocity(TICKS_PER_REV * revolutionsPerSecond);
         if (motor1 != null) motor1.setVelocity(TICKS_PER_REV * revolutionsPerSecond);
         if (motor2 != null) motor2.setVelocity(TICKS_PER_REV * revolutionsPerSecond);
+    }
+
+    private void setMotorMode(DcMotor.RunMode mode) {
+        if (motor0 != null) motor0.setMode(mode);
+        if (motor1 != null) motor1.setMode(mode);
+        if (motor2 != null) motor2.setMode(mode);
+    }
+
+    private void setMotorZeroPowerBehavior(DcMotor.ZeroPowerBehavior behavior) {
+        if (motor0 != null) motor0.setZeroPowerBehavior(behavior);
+        if (motor1 != null) motor1.setZeroPowerBehavior(behavior);
+        if (motor2 != null) motor2.setZeroPowerBehavior(behavior);
     }
 }
